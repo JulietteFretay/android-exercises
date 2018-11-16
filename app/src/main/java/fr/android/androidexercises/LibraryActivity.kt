33fde7eb9dd8341458aa55aac05982e73b06c14a
Juliette FRETAY <fretay.e1401371@etud.univ-ubs.fr>
2018-11-16 10:42:27 +0100

    retrofit

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -2,8 +2,17 @@ package fr.android.androidexercises
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import timber.log.Timber
 
 class LibraryActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -11,14 +20,33 @@ class LibraryActivity : AppCompatActivity() {
 
         // Plant logger cf. Android Timber
         // Timber.plant(new Timber.DebugTree());
+        Timber.plant(Timber.DebugTree())
 
         // TODO build Retrofit
-
+        val retrofit = Retrofit.Builder()
+                .baseUrl("http://henri-potier.xebia.fr/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
         // TODO create a service
-
+        val api = retrofit.create(HenriPotierService.Api::class.java)
         // TODO listBooks()
-
+        api.listBooks()
         // TODO enqueue call and display book title
+        api.listBooks().enqueue(object : Callback<Array<Book>> {
+            override fun onFailure(call: Call<Array<Book>>, t: Throwable) {
+                Timber.e(t)
+            }
+
+            override fun onResponse(call: Call<Array<Book>>,
+                                    response: Response<Array<Book>>) {
+                if (response.isSuccessful) {
+                    Timber.i("Success")
+                    response.body()?.forEach {
+                        Timber.i("Book " + it.title)
+                    }
+                }
+            }
+        })
 
         // TODO log books
 
@@ -26,3 +54,5 @@ class LibraryActivity : AppCompatActivity() {
     }
 
 }
+
+
